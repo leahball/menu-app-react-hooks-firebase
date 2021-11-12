65fa25b436e96119df23e5ad3f8318b4f0370432
Leah Ball <leah.ball@gmail.com>
2021-11-12 15:52:09 -0600

    messing with style and build

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Raleway', sans-serif;  
+    }
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+`
 
 const App = () => {
   return (
     <>
+    <GlobalStyle/>
+    <div>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
+      </div>
     </>
   );
 };
