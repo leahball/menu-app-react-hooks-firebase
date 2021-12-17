2a06543cccd49397f595e4b63cbdd1c7e092fdd3
iamleahb <leah.ball@gmail.com>
2021-12-17 15:21:04 -0600

    2 tests passing App js set-up

--- src/App.js
+++ src/App.js
@@ -5,25 +5,64 @@ import HomePage from "./Components/HomePage";
 import axios from "axios";
 import { GlobalStyle } from "./Styles/GlobalStyle";
 
+const initialFormValues = {
+  userName: "",
+  size: "",
+  cheese: false,
+  pepperoni: false,
+  mushrooms: false,
+  canadianBacon: false,
+  pineapple: false,
+  special: "",
+};
+
+const initialFormErrors = {
+  userName: "",
+  size: "",
+  special: "",
+};
+
+const initialOrder = [];
+const initialDisabled = true;
+
 const App = () => {
   const [orders, setOrders] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
 
-  useEffect(() => {
+  const getOrders = () => {
     axios
       .get("https://reqres.in/api/orders")
       .then((res) => {
-        console.log(res.data);
+        setOrders(res.data.data);
       })
       .catch((err) => console.log(err));
-  }, []);
+  };
+
+  const postNewOrders = (newOrder) => {
+    axios
+      .post("https://reqres.in/api/orders", newOrder)
+      .then((res) => {
+        setOrders([res.data, ...orders]);
+      })
+      .catch((err) => console.error(err))
+      .finally(() => {
+        setFormValues(initialFormValues);
+      });
+  };
 
   return (
     <div className="App">
       <header>
         <h1>Lambda Eats</h1>
         <nav>
-          <Link to="/">Home</Link>
-          <Link to="/order-pizza">Order Pizza</Link>
+          <Link to="/" id="order-pizza">
+            Home
+          </Link>
+          <Link to="/order-pizza" id="pizza-form">
+            Order Pizza
+          </Link>
         </nav>
       </header>
       <GlobalStyle />
--- src/Components/OrderForm.js
+++ src/Components/OrderForm.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function OrderForm(props) {
-  return <h2>Lambda Eats Pizza Order Form</h2>;
+  return (
+    <div className="pizza-form">
+      <h2>Lambda Eats Pizza Order Form</h2>
+    </div>
+  );
 }
 
 export default OrderForm;
